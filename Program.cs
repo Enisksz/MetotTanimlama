aea9d13c8ec2c92f1f324c3b0d47c16bce878418
Enisksz <71214539+Enisksz@users.noreply.github.com>
2022-03-10 15:03:55 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetotTanımlama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 4;  
+            Console.WriteLine(a + b);
+
+            int sonuc = Topla(a,b);
+            Console.WriteLine(sonuc);
+
+            Metotlar ornek = new Metotlar();
+            ornek.EkranaYAzdir(Convert.ToString(sonuc));
+
+            int sonuc2= ornek.ArttırVeTopla(ref a, ref b);
+            ornek.EkranaYAzdir(Convert.ToString(sonuc2));
+            ornek.EkranaYAzdir(Convert.ToString(a+b));
+
+        }
+        static int Topla( int deger1, int deger2)
+        {
+            return deger1 + deger2;
+        }
+    }
+    class Metotlar
+    {
+        public void EkranaYAzdir(string veri)
+        {
+            Console.WriteLine(veri);
+        }
+        public int ArttırVeTopla(ref int deger1,ref int deger2)
+        {
+            deger1++;
+            deger2++;
+            return deger1+deger2;
+        }
+    }
+}
